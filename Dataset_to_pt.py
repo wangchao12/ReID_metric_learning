d3e5554bb09ef9fc82fc437a24e8f68ad7da359f
zjc798719285 <798719285@qq.com>
2018-05-30 16:27:15 +0800

    Add a new file 'evluate.py', which is not complete

--- Dataset_to_pt.py
+++ Dataset_to_pt.py
@@ -23,14 +23,34 @@ def imgs_to_pt(path):
             last_id = file_i[0:4]
     return data_list
 
+def img_to_test(path):
+    file_list = []
+    files = os.listdir(path)
+    for idx, file_i in enumerate(files):
+        print(idx)
+        ids = file_i.split('_', len(file_i))
+        id = ids[0]
+        img = cv2.imread(os.path.join(path, file_i))
+        dict_i = {'image': img, 'id': id}
+        file_list.append(dict_i)
+    return file_list
+
+
 
 if __name__ == '__main__':
-    # file_path_train = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\\bounding_box_train\\'
-    file_path_test = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\\bounding_box_test\\'
-    # person_list = imgs_to_pt(path=file_path_train)
-    # torch.save(person_list, 'traindata.pt')
-    person_list2 = imgs_to_pt(path=file_path_test)
-    torch.save(person_list2[4:-1], 'testdata.pt')
-    # print(np.shape(person_list))
+    # # file_path_train = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\\bounding_box_train\\'
+    # file_path_test = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\\bounding_box_test\\'
+    # # person_list = imgs_to_pt(path=file_path_train)
+    # # torch.save(person_list, 'traindata.pt')
+    # person_list2 = imgs_to_pt(path=file_path_test)
+    # torch.save(person_list2[4:-1], 'testdata.pt')
+    # # print(np.shape(person_list))
+    query_path = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\query\\'
+    gallary_path = 'E:\Person_ReID\DataSet\Market-1501-v15.09.15\\bounding_box_test\\'
+    query_list = img_to_test(query_path)
+    gallary_list = img_to_test(gallary_path)
+    torch.save(query_list, 'query.pt')
+    torch.save(gallary_list, 'gallary.pt')
+
 
 
---
+++ evluate.py
@@ -0,0 +1,37 @@
+from models.mobilenet import MobileNetV2
+import torch
+import numpy as np
+import time
+import scipy.io as sio
+
+
+model = MobileNetV2().to('cuda')
+
+def extract_fc(query, name):
+    query_list = []
+    for idx, query_i in enumerate(query):
+        try:
+
+            img = query_i['image']
+            id = query_i['id']
+            img2 = np.expand_dims(np.transpose(img, [2, 0, 1]), axis=0)
+            t1 = time.time()
+            fc = model.forward(torch.cuda.FloatTensor(img2))
+            t2 = time.time()
+            dict_i = {'img': img, 'fc': fc.cpu().detach().numpy(), 'id': id}
+            query_list.append(dict_i)
+            print(idx, 'time:', t2 - t1)
+        except:
+            continue
+    return {name: query_list}
+
+
+if __name__ =='__main__':
+    query = torch.load('.\dataset\query.pt')
+    gallary = torch.load('.\dataset\gallary.pt')
+    gallary_fc = extract_fc(gallary, 'gallary')
+    query_fc = extract_fc(query, 'query')
+    sio.savemat('./checkpoint/query_fc.mat', query_fc)
+    sio.savemat('./checkpoint/gallary_fc.mat', gallary_fc)
+
+
